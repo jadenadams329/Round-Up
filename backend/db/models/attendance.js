ba2c7f4e40dc61823a0a618edd91f4f4216b9365
Jaden Adams <jadenadams329@gmail.com>
2023-12-02 14:04:33 -0800

    added get all attendees and request attendance

--- backend/db/models/attendance.js
+++ backend/db/models/attendance.js
@@ -11,6 +11,20 @@ module.exports = (sequelize, DataTypes) => {
 			Attendance.belongsTo(models.Event, { foreignKey: "eventId" });
 			Attendance.belongsTo(models.User, { foreignKey: "userId" });
 		}
+
+		static organizeAttendees(result){
+			const attendees = result.map((attendee)=> {
+				return {
+					id: attendee.id,
+					firstName: attendee.firstName,
+					lastName: attendee.lastName,
+					Attendance: {
+						status: attendee['Attendances.status']
+					}
+				}
+			})
+			return attendees
+		}
 	}
 	Attendance.init(
 		{
--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,8 +1,124 @@
 const express = require("express");
-const { Event, Event_Image, User, Venue } = require("../../db/models");
+const {
+	Event,
+	Event_Image,
+	User,
+	Venue,
+	Attendance,
+} = require("../../db/models");
 const { validateEvent } = require("../../utils/validation");
 const { requireAuth } = require("../../utils/auth");
+const attendance = require("../../db/models/attendance");
 const router = express.Router();
+
+//Request to Attend an Event based on the Event's id
+router.post("/:eventId/attendance", requireAuth, async (req, res, next) => {
+	try {
+		const { user } = req;
+		const eventId = req.params.eventId;
+
+		let userMembership = false;
+		//check if event exist AND grab groupId
+		const event = await Event.findByPk(eventId);
+		if (!event) {
+			const err = new Error("Event couldn't be found");
+			err.status = 404;
+			return next(err);
+		}
+
+		//check to see if user is member of group
+		const isMember = await User.scope({
+			method: ["isMember", user.id, event.groupId],
+		}).findOne();
+		if (isMember) {
+			if (
+				isMember["Memberships.status"] === "member" ||
+				isMember["Memberships.status"] === "co-host"
+			) {
+				userMembership = true;
+			}
+		}
+
+		//check to see if user is pending or already attending event
+		const isGoing = await Attendance.findOne({
+			where: {
+				userId: user.id,
+				eventId: eventId,
+				status: ["pending", "attending", "waitlist"],
+			},
+		});
+		if (isGoing) {
+			if (isGoing.status === "pending" || isGoing.status === "waitlist") {
+				const err = new Error("Attendance has already been requested");
+				err.status = 400;
+				return next(err);
+			}
+			if (isGoing.status === "attending") {
+				const err = new Error("User is already an attendee of the event");
+				err.status = 400;
+				return next(err);
+			}
+		} else {
+			if (userMembership) {
+				const requestAttendance = await Attendance.create({
+					userId: user.id,
+					eventId: eventId,
+					status: "pending",
+				});
+
+				return res.json({
+					userId: requestAttendance.userId,
+					status: requestAttendance.status,
+				});
+			} else {
+				const err = new Error("Forbidden");
+				err.status = 403;
+				return next(err);
+			}
+		}
+	} catch (err) {
+		next(err);
+	}
+});
+
+//Get all Attendees of an Event specified by its id
+router.get("/:eventId/attendees", async (req, res, next) => {
+	try {
+		const { user } = req;
+		const eventId = req.params.eventId;
+
+		let scopeMethod = "allAttendees";
+
+		//check if event exist AND grab groupId
+		const event = await Event.findByPk(eventId);
+		if (!event) {
+			const err = new Error("Event couldn't be found");
+			err.status = 404;
+			return next(err);
+		}
+
+		//check to see if a user is logged in
+		if (user) {
+			//check user roles
+			const roles = await User.scope({
+				method: ["isOrganizerOrCoHost", user.id, event.groupId],
+			}).findOne();
+
+			if (roles.Groups.length >= 1 || roles.Memberships.length >= 1) {
+				scopeMethod = "allAttendeesAuthorized";
+			}
+		}
+
+		const result = await User.scope({
+			method: [scopeMethod, eventId],
+		}).findAll();
+
+		const attendees = Attendance.organizeAttendees(result);
+		return res.json({ Attendees: attendees });
+	} catch (err) {
+		next(err);
+	}
+});
 
 //Add an Image to an Event based on the Event's id
 router.post("/:eventId/images", requireAuth, async (req, res, next) => {
@@ -30,11 +146,11 @@ router.post("/:eventId/images", requireAuth, async (req, res, next) => {
 
 		//add image to event
 		const addImage = await Event_Image.create({ eventId, url, preview });
-        return res.json({
-            id: addImage.id,
-            url: addImage.url,
-            preview: addImage.preview
-        })
+		return res.json({
+			id: addImage.id,
+			url: addImage.url,
+			preview: addImage.preview,
+		});
 	} catch (err) {
 		next(err);
 	}
