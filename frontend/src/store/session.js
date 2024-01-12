d3a19be1c4717bf86b123e5bf24b8fad320cac6f
Jaden Adams <jadenadams329@gmail.com>
2024-01-12 12:24:56 -0800

    sign up form done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -17,23 +17,40 @@ export const removeUser = () => ({
 /** Thunk Action Creators: */
 export const login = (user) => async (dispatch) => {
 	const { credential, password } = user;
-	const response = await csrfFetch("/api/session", {
+	const res = await csrfFetch("/api/session", {
 		method: "POST",
 		body: JSON.stringify({
 			credential,
 			password,
 		}),
 	});
-	const data = await response.json();
+	const data = await res.json();
 	dispatch(setUser(data.user));
-	return response;
+	return res;
 };
 
 export const restoreUser = () => async (dispatch) => {
-	const response = await csrfFetch("/api/session");
-	const data = await response.json();
+	const res = await csrfFetch("/api/session");
+	const data = await res.json();
 	dispatch(setUser(data.user));
-	return response;
+	return res;
+};
+
+export const signup = (user) => async (dispatch) => {
+	const { username, firstName, lastName, email, password } = user;
+	const res = await csrfFetch("/api/users", {
+		method: "POST",
+		body: JSON.stringify({
+			username,
+			firstName,
+			lastName,
+			email,
+			password,
+		}),
+	});
+	const data = await res.json();
+	dispatch(setUser(data.user));
+	return res;
 };
 
 const initialState = {
